d3b7d2afed62d98e6ff176d69da8bb343bf9f1ac
Logan <146642263+logan2d5@users.noreply.github.com>
2024-09-21 23:14:17 +0800

    chore: add vue-router

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -2,7 +2,11 @@
 </script>
 
 <template>
-  <h2 class="text-sky-500">Hello, world!</h2>
+  <ul>
+    <li><router-link to="/">Home</router-link></li>
+    <li><router-link to="/about">About</router-link></li>
+  </ul>
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
